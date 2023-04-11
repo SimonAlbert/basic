a9fa2a5f389871311261bace31d51bf3a26bc8a4
Simon <2653195657@qq.com>
2023-04-12 02:15:37 +0800

    排序和树

--- Tree/search_tree.c
+++ Tree/search_tree.c
@@ -21,14 +21,16 @@ node_ptr lc(node_ptr t) {
 node_ptr rc(node_ptr t) {
     return t->right;
 }
-
+int end(node_ptr t){
+    return !t||!t->count;
+}
 void print_node(node_ptr t) {
     printf("%d", t->val);
 }
 
 // 根左右
 void pre_order_print(node_ptr t) {
-    if (!t) return;
+    if (end(t)) return;
     print_node(t);
     pre_order_print(lc(t));
     pre_order_print(rc(t));
@@ -36,7 +38,7 @@ void pre_order_print(node_ptr t) {
 
 // 左根右
 void in_order_print(node_ptr t) {
-    if (!t) return;
+    if (end(t)) return;
     in_order_print(lc(t));
     print_node(t);
     in_order_print(rc(t));
@@ -44,18 +46,19 @@ void in_order_print(node_ptr t) {
 
 // 左右根
 void post_order_print(node_ptr t) {
-    if (!t) return;
+    if (end(t)) return;
     post_order_print(lc(t));
     post_order_print(rc(t));
     print_node(t);
 }
 
 // 插入
-void insert(T *v, node_ptr t) {
-    if (!t) {
+void insert(T *v, node_ptr t, node_ptr parent) {
+    if (end(t)) {
         t = (node_ptr) malloc(sizeof(node));
         t->val = *v;
         t->count = 1;
+        t->parent = parent;
         t->left = NULL;
         t->right = NULL;
         return;
@@ -63,36 +66,50 @@ void insert(T *v, node_ptr t) {
     if (*v == t->val) {
         t->count++;
     } else if (*v < t->val) {
-        insert(v, lc(t));
+        insert(v, lc(t), t);
     } else if (*v > t->val) {
-        insert(v, rc(t));
+        insert(v, rc(t), t);
     }
 }
-node_ptr find_min(node_ptr t){
-    if(!lc(t)) return t;
+
+node_ptr find_min(node_ptr t) {
+    if (!lc(t)) return t;
     return find_min(lc(t));
 }
-node_ptr find_max(node_ptr t){
-    if(!rc(t)) return t;
+
+node_ptr find_max(node_ptr t) {
+    if (!rc(t)) return t;
     return find_max(rc(t));
 }
-node_ptr find_val(T* v, node_ptr t){
-    if(!t) return NULL;
 
-    if(*v == t->val){
+node_ptr find_val(T *v, node_ptr t) {
+    if (end(t)) return NULL;
+    if (*v == t->val) {
         return t;
-    }else if(*v < t->val){
+    } else if (*v < t->val) {
         return find_val(v, lc(t));
-    }else{// *v > t->val
+    } else {// *v > t->val
         return find_val(v, rc(t));
     }
 }
-//
-void delete(T* v, node_ptr t){
-    node_ptr p = find_val(v, t);
-    if(!p) return;
-    // 叶子节点
-    if(!lc(p) && !rc(p)){
 
+void delete_val(T *v, node_ptr t) {
+    if (end(t)) return;
+    if (*v == t->val) {
+        if (t->count > 1) {
+            t->count--;
+            return;
+        } else {
+            node_ptr l = lc(t);
+            node_ptr r = rc(t);
+            node_ptr c = l?l:r;
+            if(l && !r){
+
+            }
+        }
+    } else if (*v < t->val) {
+        delete_val(v, lc(t));
+    } else {
+        delete_val(v, rc(t));
     }
 }
