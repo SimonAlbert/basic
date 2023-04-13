3cfa3e0524c90a0d495b3ba67a473a3447716876
Simon <2653195657@qq.com>
2023-04-14 00:41:58 +0800

    fix AVL四种元问题修正

--- Tree/AVL_tree.c
+++ Tree/AVL_tree.c
@@ -4,25 +4,25 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef int T;
 
 typedef struct node {
     T val;
     int count;
-//    int size;
-//    int height;
-    int bf;
+    int height;
     struct node *left;
     struct node *right;
 } *node_ptr, node;
+node_ptr root = NULL;
 void rotate_right(node_ptr);
 void rotate_left(node_ptr);
 node_ptr lc(node_ptr t) {
-    return t->left;
+    return t?t->left:NULL;
 }
 node_ptr rc(node_ptr t) {
-    return t->right;
+    return t?t->right:NULL;
 }
 
 void set_lc(node_ptr t, node_ptr l){
@@ -35,10 +35,16 @@ void set_rc(node_ptr t, node_ptr r){
 int end(node_ptr t) {
     return !t;
 }
+int height(node_ptr t){
+    return t?t->height:0;
+}
+void updateHeight(node_ptr t){
+    t->height = height(lc(t)) > height(rc(t))? height(lc(t)) + 1: height(rc(t)) + 1;
+}
 
-//void updateSize(node_ptr t){
-//    t->size = lc(t)->size + rc(t)->size;
-//}
+int balanceFactor(node_ptr t){
+    return abs(height(lc(t)) - height(rc(t)));
+}
 
 void print_node(node_ptr t) {
     printf("{%d}", t->val);
@@ -81,9 +87,15 @@ void linkedlist_format_output(node_ptr t, int deepth){
     for (int i = 0; i < deepth; ++i) {
         printf("       ");
     }
-    printf("%d\n", t->val);
+    printf("%d:%d\n", t->val, t->height);
 
     linkedlist_format_output(lc(t), deepth+1);
+}
+
+void print(node_ptr root_){
+    printf("========format output========\n");
+    linkedlist_format_output(root_, 0);
+    printf("=============================\n");
 }
 // 插入
 node_ptr insert(node_ptr t, T *v, int* lr_log) {
@@ -91,42 +103,38 @@ node_ptr insert(node_ptr t, T *v, int* lr_log) {
         node_ptr p = (node_ptr)malloc(sizeof(node));
         p->val = *v;
         p->count = 1;
-//        p->size = 1;
-        p->bf = 0;
+        p->height = 1;
         p->left = NULL;
         p->right = NULL;
         return p;
     }
     if (*v == t->val) {
         t->count++;
+        return t;
     } else if (*v < t->val) {
-        *lr_log = *lr_log * 2;
-        t->bf --;
         set_lc(t, insert(lc(t), v, lr_log));
+        *lr_log = *lr_log * 2;
     } else if (*v > t->val) {
-        *lr_log = *lr_log * 2 + 1;
-        t->bf ++;
         set_rc(t, insert(rc(t), v, lr_log));
+        *lr_log = *lr_log * 2 + 1;
     }
-    int flag = 0b11&*lr_log; // 00:LL 01:LR 10:RL 11:RR
-    if(t->bf < -1){
+    updateHeight(t);
+    int flag = 0b11&*lr_log; // 00:LL 10:LR 01:RL 11:RR
+    if(balanceFactor(t) > 1){
         // 偏左
         if(flag == 0b00){
             rotate_right(t);
-        }else if (flag == 0b01){
+        }else if (flag == 0b10){
             rotate_left(lc(t));
             rotate_right(t);
         }
-        t->bf += 2;
-    }else if(t->bf > 1){
         // 偏右
-        if(flag == 0b11){
+        else if(flag == 0b11){
             rotate_left(t);
-        }else if (flag == 0b10){
+        }else if (flag == 0b01){
             rotate_right(rc(t));
             rotate_left(t);
         }
-        t->bf -= 2;
     }
     return t;
 }
@@ -181,12 +189,11 @@ node_ptr delete_val(node_ptr t, T *v, int* lr_log) {
     } else if (*v < t->val) {
         set_lc(t, delete_val(lc(t), v, lr_log));
         *lr_log = *lr_log * 2 + 1;
-        t->bf ++;
     } else {
         set_rc(t, delete_val(rc(t), v, lr_log));
         *lr_log = *lr_log * 2;
-        t->bf --;
     }
+    updateHeight(t);
     return t;
 }
 
@@ -204,6 +211,9 @@ node_ptr delete_val(node_ptr t, T *v, int* lr_log) {
  *                                       F   G
  */
 void rotate_right(node_ptr newB_oldA){
+    printf("before right rotate: %d\n", newB_oldA->val);
+    print(root);
+//    print(newB_oldA);
     // 不修改指针, 修改指针指向的值
     node_ptr oldB = lc(newB_oldA);
     node_ptr E = rc(oldB);
@@ -212,6 +222,11 @@ void rotate_right(node_ptr newB_oldA){
     *newB_oldA = *oldB;
     set_lc(newA, E);
     set_rc(newB_oldA, newA);
+    updateHeight(newA);
+    updateHeight(newB_oldA);
+    printf("after right rotate\n");
+//    print(newB_oldA);
+    print(root);
 }
 
 /* 节点A左旋
@@ -227,6 +242,9 @@ void rotate_right(node_ptr newB_oldA){
  *                           D    E
  */
 void rotate_left(node_ptr newC_oldA){
+    printf("before left rotate: %d\n", newC_oldA->val);
+    print(root);
+//    print(newC_oldA);
     node_ptr oldC = rc(newC_oldA);
     node_ptr F = lc(oldC);
     node_ptr newA = (node_ptr) malloc(sizeof(node));
@@ -234,31 +252,32 @@ void rotate_left(node_ptr newC_oldA){
     *newC_oldA = *oldC;
     set_rc(newA, F);
     set_lc(newC_oldA, newA);
+    updateHeight(newA);
+    updateHeight(newC_oldA);
+    printf("after left rotate\n");
+//    print(newC_oldA);
+    print(root);
 }
 
-void print(node_ptr root){
-    printf("========format output========\n");
-    linkedlist_format_output(root, 0);
-    printf("=============================\n");
-}
 int main()
 {
-    int _i = 0b0101;
-    printf("%d\n", _i);
-    node_ptr root = NULL;
+//    node_ptr root = NULL;
     T in = 1;
+    int lr_log=0b00;
+    int x[16] = {5,3,6,7,4,2,1,-5,-3, -1, -7,-19, -99, -110, -111, -105};
+    for (int i = 0; i < 16; ++i) {
+        root = insert(root, x+i, &lr_log);
+    }
     while(in != '0'){
         scanf("%d", &in);
+//        system("cls");
         if (!in) { break; }
-        int lr_log=0b00;
+        printf("before insert\n");
+        print(root);
+        lr_log=0b00;
         root = insert(root, &in, &lr_log);
+        printf("after insert\n");
         print(root);
     }
-//    T d = 3;
-//    root = delete_val(root, &d, 0);
-//    rotate_right(root);
-//    print(root);
-//    rotate_left(rc(rc(root)));
-//    print(root);
     return 0;
 }
