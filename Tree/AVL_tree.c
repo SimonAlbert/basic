e06ad419d4a90c58094d70efe48fbce142b5dadf
cui xipeng <2653195657@qq.com>
2023-04-14 16:38:17 +0800

    修复AVL删除的bug

--- Tree/AVL_tree.c
+++ Tree/AVL_tree.c
@@ -25,27 +25,49 @@ node_ptr rc(node_ptr t) {
     return t?t->right:NULL;
 }
 
-void set_lc(node_ptr t, node_ptr l){
-    t->left = l;
-
-}
-void set_rc(node_ptr t, node_ptr r){
-    t->right = r;
-}
-int end(node_ptr t) {
-    return !t;
-}
 int height(node_ptr t){
     return t?t->height:0;
 }
 void updateHeight(node_ptr t){
     t->height = height(lc(t)) > height(rc(t))? height(lc(t)) + 1: height(rc(t)) + 1;
 }
+void set_lc(node_ptr t, node_ptr l){
+    t->left = l;
+    updateHeight(t);
+
+}
+void set_rc(node_ptr t, node_ptr r){
+    t->right = r;
+    updateHeight(t);
+}
+int end(node_ptr t) {
+    return !t;
+}
 
 int balanceFactor(node_ptr t){
     return abs(height(lc(t)) - height(rc(t)));
 }
-
+void _situtation(node_ptr t, int deepth, int* lr_log){
+    if(height(lc(t)) > height(rc(t))){
+        if(deepth < 2){
+            _situtation(lc(t), deepth + 1, lr_log);
+        }
+        *lr_log *= 2;
+    }else if(height(lc(t)) < height(rc(t))){
+        if(deepth < 2){
+            _situtation(rc(t), deepth + 1, lr_log);
+        }
+        *lr_log = *lr_log * 2 + 1;
+    }
+}
+int situtation(node_ptr t){
+    if(height(t) < 2 || balanceFactor(t) < 1){
+        return -1;
+    }
+    int deepth = 0,lr_log=0;
+    _situtation(t, deepth, &lr_log);
+    return lr_log;
+}
 void print_node(node_ptr t) {
     printf("{%d}", t->val);
 }
@@ -87,7 +109,7 @@ void linkedlist_format_output(node_ptr t, int deepth){
     for (int i = 0; i < deepth; ++i) {
         printf("       ");
     }
-    printf("%d:%d\n", t->val, t->height);
+    printf("%d:%d\n", t->val, height(t));
 
     linkedlist_format_output(lc(t), deepth+1);
 }
@@ -118,7 +140,6 @@ node_ptr insert(node_ptr t, T *v, int* lr_log) {
         set_rc(t, insert(rc(t), v, lr_log));
         *lr_log = *lr_log * 2 + 1;
     }
-    updateHeight(t);
     int flag = 0b11&*lr_log; // 00:LL 10:LR 01:RL 11:RR
     if(balanceFactor(t) > 1){
         // 偏左
@@ -148,7 +169,25 @@ node_ptr find_max(node_ptr t) {
     if (!rc(t)) return t;
     return find_max(rc(t));
 }
-
+node_ptr delete_max(node_ptr t){
+    if(!rc(t)) {// 到达要删除的节点
+        node_ptr  l = lc(t);
+        free(t);
+        return l;
+    }
+    set_rc(t, delete_max(rc(t)));
+    return t;
+}
+node_ptr delete_min(node_ptr t){
+    if(!lc(t)) {// 到达要删除的节点
+        if(rc(t)){
+            return rc(t);
+        }
+        return NULL;
+    }
+    set_lc(t, delete_min(lc(t)));
+    return t;
+}
 node_ptr find_val(node_ptr t, T *v) {
     if (end(t)) return NULL;
     if (*v == t->val) {
@@ -162,9 +201,7 @@ node_ptr find_val(node_ptr t, T *v) {
 
 
 // 使用链表实现
-node_ptr delete_val(node_ptr t, T *v, int* lr_log);
-
-node_ptr delete_val(node_ptr t, T *v, int* lr_log) {
+node_ptr delete_val(node_ptr t, T *v) {
     if (end(t)) return NULL;
     if (*v == t->val) {
         if (t->count > 1) {
@@ -174,10 +211,21 @@ node_ptr delete_val(node_ptr t, T *v, int* lr_log) {
             node_ptr r = rc(t);
             node_ptr child = l ? l : r;
             if (l && r) {
-                node_ptr _t = find_max(lc(t));
-                set_rc(_t, rc(t));
-                free(t);
-                return _t;
+                /*       |                      |
+                 *       t        ===>     predecessor
+                 *      / \                    / \
+                 *     l   r                  l   r
+                 *      \                      \
+                 *       lr                    lr
+                 *      /  \                  /
+                 *     lrl  predecessor     lrl
+                 */
+                node_ptr pre = find_max(l);
+                *t = *pre;
+                set_rc(t, r);
+                set_lc(t, l);
+                set_lc(t, delete_max(l));
+                return t;
             } else if (!child) {
                 free(t);
                 return NULL;
@@ -187,13 +235,29 @@ node_ptr delete_val(node_ptr t, T *v, int* lr_log) {
             }
         }
     } else if (*v < t->val) {
-        set_lc(t, delete_val(lc(t), v, lr_log));
-        *lr_log = *lr_log * 2 + 1;
+        set_lc(t, delete_val(lc(t), v));
     } else {
-        set_rc(t, delete_val(rc(t), v, lr_log));
-        *lr_log = *lr_log * 2;
+        set_rc(t, delete_val(rc(t), v));
     }
-    updateHeight(t);
+    int flag = situtation(t);
+    printf("%d situation:%d\n", t->val, flag);
+    print(t);
+    if(balanceFactor(t) > 1){
+        // 偏左
+        if(flag == 0b00){
+            rotate_right(t);
+        }else if (flag == 0b10){
+            rotate_left(lc(t));
+            rotate_right(t);
+        }
+            // 偏右
+        else if(flag == 0b11){
+            rotate_left(t);
+        }else if (flag == 0b01){
+            rotate_right(rc(t));
+            rotate_left(t);
+        }
+    }
     return t;
 }
 
@@ -222,8 +286,6 @@ void rotate_right(node_ptr newB_oldA){
     *newB_oldA = *oldB;
     set_lc(newA, E);
     set_rc(newB_oldA, newA);
-    updateHeight(newA);
-    updateHeight(newB_oldA);
     printf("after right rotate\n");
 //    print(newB_oldA);
     print(root);
@@ -252,8 +314,6 @@ void rotate_left(node_ptr newC_oldA){
     *newC_oldA = *oldC;
     set_rc(newA, F);
     set_lc(newC_oldA, newA);
-    updateHeight(newA);
-    updateHeight(newC_oldA);
     printf("after left rotate\n");
 //    print(newC_oldA);
     print(root);
@@ -264,19 +324,29 @@ int main()
 //    node_ptr root = NULL;
     T in = 1;
     int lr_log=0b00;
-    int x[16] = {5,3,6,7,4,2,1,-5,-3, -1, -7,-19, -99, -110, -111, -105};
-    for (int i = 0; i < 16; ++i) {
+//    int x[16] = {5,3,6,7,4,2,1,-5,-3, -1, -7,-19, -99, -110, -111, -105};
+    int x[6] = {5, 3, 7, 4, 2, 6};
+    for (int i = 0; i < 6; ++i) {
         root = insert(root, x+i, &lr_log);
     }
+//    while(in != '0'){
+//        scanf("%d", &in);
+////        system("cls");
+//        if (!in) { break; }
+//        printf("before insert\n");
+//        print(root);
+//        lr_log=0b00;
+//        root = insert(root, &in, &lr_log);
+//        printf("after insert\n");
+//        print(root);
+//    }
     while(in != '0'){
         scanf("%d", &in);
-//        system("cls");
         if (!in) { break; }
-        printf("before insert\n");
+        printf("before delete\n");
         print(root);
-        lr_log=0b00;
-        root = insert(root, &in, &lr_log);
-        printf("after insert\n");
+        root = delete_val(root, &in);
+        printf("after delete\n");
         print(root);
     }
     return 0;
