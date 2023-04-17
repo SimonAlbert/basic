0f7baf486326ea7b771792c01f6d24a41cbc70a6
Simon <2653195657@qq.com>
2023-04-18 04:11:12 +0800

    B-Tree 搜索和插入

--- Tree/B_Tree.c
+++ Tree/B_Tree.c
@@ -1,6 +1,8 @@
 //
 // Created by Simon on 2023/4/17.
 //
+#include <stdio.h>
+#include <stdlib.h>
 /*
  * B-tree
  * M B树的阶
@@ -15,13 +17,50 @@
  *     / | \    / \
  *    E  F G   H   I
  */
-
+typedef int key_type;
 #define M 4 // M=4, 2-3-4 Tree
 int maxsize = M-1;
 int minsize = M/2;
 typedef struct TreeNode{
     int size;
-    int keys[M-1];
+    key_type keys[M-1];
     struct TreeNode* children[M];
+    int is_leaf;
 } *pTreeNode, TreeNode;
 
+void insert(pTreeNode, key_type*);
+
+pTreeNode find(pTreeNode t, key_type* v)
+{
+    // 空树
+
+    // 搜索
+    key_type tmp;
+    for (int i = 0; i < t->size; ++i) {
+        tmp = t->keys[i];
+        if(tmp == *v){
+            return t;
+        }else if(tmp < *v && t->keys[i+1] > *v){
+            return find(t->children[i], v);
+        }
+    }
+    return find(t->children[t->size-1], v);
+}
+
+void insert(pTreeNode t, key_type* v)
+{
+    // 叶节点
+    if(t->is_leaf){
+
+    }else{
+        for (int i = 0; i < t->size; ++i) {
+            if(t->keys[i] < *v && t->keys[i+1] > *v){
+                insert(t->children[i], v);
+            }
+        }
+        insert(t->children[t->size - 1], v);
+    }
+}
+int main(){
+
+}
