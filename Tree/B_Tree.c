1021b50082954e2103d30b32c29e59d1b4174d9d
cui xipeng <2653195657@qq.com>
2023-04-20 18:02:21 +0800

    B-Tree 插入

--- Tree/B_Tree.c
+++ Tree/B_Tree.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+
 /*
  * B-tree 数组实现
  * M B树的阶
@@ -19,33 +20,31 @@
  */
 typedef int key_type;
 #define M 4 // M=4, 2-3-4 Tree
-int maxsize = M-1;
-int minsize = M/2;
-typedef struct TreeNode{
+int maxsize = M - 1;
+int minsize = M / 2;
+typedef struct TreeNode {
     int size;
     key_type keys[M];
-    struct TreeNode* children[M];
+    struct TreeNode *children[M];
     int is_leaf;
 } *pTreeNode, TreeNode;
 
-void insert(pTreeNode, key_type*);
+void insert(pTreeNode, key_type *);
 
-void exchange(key_type* a, key_type* b)
-{
+void exchange(key_type *a, key_type *b) {
     key_type tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
 // 数组插入
-int key_into_node(key_type* arr, int num, key_type *v)
-{
+int key_into_node(key_type *arr, int num, key_type *v) {
     // 节点满
-    if(num == M) return -1;
+    if (num == M) return -1;
     // 遍历找到目标位置(下标)
     int position;
     for (int i = 0; i < num; ++i) {
-        if(arr[i] > *v){
+        if (arr[i] > *v) {
             position = i;
             break;
         }
@@ -56,37 +55,47 @@ int key_into_node(key_type* arr, int num, key_type *v)
     }
 }
 
-pTreeNode find(pTreeNode t, key_type* v)
-{
+pTreeNode find(pTreeNode t, key_type *v) {
     // 空树
 
     // 找到新key的位置
     key_type tmp;
     for (int i = 0; i < t->size; ++i) {
         tmp = t->keys[i];
-        if(tmp == *v){
+        if (tmp == *v) {
             return t;
-        }else if(tmp < *v && t->keys[i+1] > *v){
+        } else if (tmp < *v && t->keys[i + 1] > *v) {
             return find(t->children[i], v);
         }
     }
-    return find(t->children[t->size-1], v);
+    return find(t->children[t->size - 1], v);
 }
 
-void insert(pTreeNode t, key_type* v)
-{
+/* 只能插入叶子节点
+ * 叶子节点的几种情况
+ * 1. 叶子节点未满
+ *     直接数组插入即可
+ * 2. 叶子节点已满
+ *     2.1 兄弟节点未满
+ *         与父节点交换key
+ */
+key_type* insert(pTreeNode t, key_type *v) {
     // 叶节点
-    if(t->is_leaf){
+    if (t->is_leaf) {
+        // 节点已满, 插入元素, 返回指向最后一个key的指针, 由父节点将溢出的元素推给兄弟节点
+        // 节点未满, 直接插入, 返回NULL
+    } else {
 
-    }else{
         for (int i = 0; i < t->size; ++i) {
-            if(t->keys[i] < *v && t->keys[i+1] > *v){
+            // 找到插入的子节点
+            if (t->keys[i] < *v && t->keys[i + 1] > *v) {
                 insert(t->children[i], v);
             }
         }
         insert(t->children[t->size - 1], v);
     }
 }
-int main(){
+
+int main() {
 
 }
